01f11b31c64e0eaca63395f1aa372cffc3f14d48
Mingzhe Zou <zoumingzhe@qq.com>
2024-01-05 15:55:09 +0800

    setup: init

---
+++ xmonitor/utils/attribute.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__name__ = "xmonitor"
+__version__ = "0.1.alpha.1"
+__description__ = "Monitor tool and log file management."
+__url_home__ = "https://github.com/bondbox/xmonitor"
+__url_code__ = __url_home__
+__url_docs__ = __url_home__
+__url_bugs__ = urljoin(__url_home__, "issues")
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
