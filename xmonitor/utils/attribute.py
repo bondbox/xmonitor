02eff330009552562b2bea52214cda312ae64889
Mingzhe Zou <zoumingzhe@qq.com>
2024-01-14 16:53:47 +0800

    version: 0.1.1

--- xmonitor/utils/attribute.py
+++ xmonitor/utils/attribute.py
@@ -3,7 +3,7 @@
 from urllib.parse import urljoin
 
 __name__ = "xmonitor"
-__version__ = "0.1"
+__version__ = "0.1.1"
 __description__ = "Monitor tool and log file management."
 __url_home__ = "https://github.com/bondbox/xmonitor"
 __url_code__ = __url_home__
