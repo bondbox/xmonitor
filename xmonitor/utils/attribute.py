58a6b9402ffd7f0465768bfb6ce765a814bc8fd0
Mingzhe Zou <zoumingzhe@qq.com>
2024-02-27 17:20:05 +0800

    setup: attribute: fix __url_bugs__ error

--- xmonitor/utils/attribute.py
+++ xmonitor/utils/attribute.py
@@ -5,7 +5,7 @@ from urllib.parse import urljoin
 __name__ = "xmonitor"
 __version__ = "0.2"
 __description__ = "Monitor tool and log file management."
-__url_home__ = "https://github.com/bondbox/xmonitor"
+__url_home__ = "https://github.com/bondbox/xmonitor/"
 __url_code__ = __url_home__
 __url_docs__ = __url_home__
 __url_bugs__ = urljoin(__url_home__, "issues")
